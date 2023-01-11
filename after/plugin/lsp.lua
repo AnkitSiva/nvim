130ab68bf365a85ed60a77889191376eec6a65bb
Ankit Siva <ankitsiv@amazon.com>
2023-01-11 10:27:48 -0800

    lua stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -68,6 +68,14 @@ require'lspconfig'.sumneko_lua.setup {
 	on_new_config = (function(new_config, new_root_dir)
 		new_config.cmd = require'lspcontainers'.command(server, { root_dir = new_root_dir })
 	end),
+    root_dir = require'lspconfig/util'.root_pattern(".git", vim.fn.getcwd()),
     on_attach = on_attach,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {'vim'},
+            },
+        },
+    },
 }
 
