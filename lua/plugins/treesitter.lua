f364a30d6712b74b89747c4e70f3854cf77bbfcb
Ankit Siva <ankitsiv@amazon.com>
2025-02-06 22:19:02 +0000

    updated colorscheme and treesitter langs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all" (the four listed parsers should always be installed)
-            ensure_installed = { "c", "python", "kotlin", "bash", "dockerfile", "java", "latex", "lua"},
+            ensure_installed = { "c", "python", "kotlin", "bash", "dockerfile", "java", "lua"},
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
